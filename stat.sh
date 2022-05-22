963369b07173adcfbad70e5ca46a1c162eed7c76
cryptoscheme <97607858+cryptoscheme@users.noreply.github.com>
2022-05-22 22:51:13 +0800

    Update stat.sh

--- stat.sh
+++ stat.sh
@@ -13,26 +13,19 @@ fi
 # level 0
 benchmarkFuncName[0]=BenchmarkSchemeL0Setup
 benchmarkFuncName[1]=BenchmarkSchemeL0RootWalletKeyGen
-#benchmarkFuncName[2]=BenchmarkSchemeL0VerifyKeyDerive
-#benchmarkFuncName[3]=BenchmarkSchemeL0VerifyKeyCheck
-#benchmarkFuncName[4]=BenchmarkSchemeL0SignKeyDerive
-#benchmarkFuncName[5]=BenchmarkSchemeL0Sign
-#benchmarkFuncName[6]=BenchmarkSchemeL0Verify
+benchmarkFuncName[2]=BenchmarkSchemeL0VerifyKeyDerive
+benchmarkFuncName[3]=BenchmarkSchemeL0VerifyKeyCheck
+benchmarkFuncName[4]=BenchmarkSchemeL0SignKeyDerive
+benchmarkFuncName[5]=BenchmarkSchemeL0Sign
+benchmarkFuncName[6]=BenchmarkSchemeL0Verify
 
 #level 1
-benchmarkFuncName[2]=BenchmarkSchemeL1WalletKeyDelegate
-benchmarkFuncName[3]=BenchmarkSchemeL1VerifyKeyDerive
-benchmarkFuncName[4]=BenchmarkSchemeL1VerifyKeyCheck
-benchmarkFuncName[5]=BenchmarkSchemeL1SignKeyDerive
-benchmarkFuncName[6]=BenchmarkSchemeL1Sign
-benchmarkFuncName[7]=BenchmarkSchemeL1Verify
-#benchmarkFuncName[7]=BenchmarkSchemeL1WalletKeyDelegate
-#benchmarkFuncName[8]=BenchmarkSchemeL1VerifyKeyDerive
-#benchmarkFuncName[9]=BenchmarkSchemeL1VerifyKeyCheck
-#benchmarkFuncName[11]=BenchmarkSchemeL1SignKeyDerive
-#benchmarkFuncName[12]=BenchmarkSchemeL1Sign
-#benchmarkFuncName[13]=BenchmarkSchemeL1Verify
-
+benchmarkFuncName[7]=BenchmarkSchemeL1WalletKeyDelegate
+benchmarkFuncName[8]=BenchmarkSchemeL1VerifyKeyDerive
+benchmarkFuncName[9]=BenchmarkSchemeL1VerifyKeyCheck
+benchmarkFuncName[10]=BenchmarkSchemeL1SignKeyDerive
+benchmarkFuncName[11]=BenchmarkSchemeL1Sign
+benchmarkFuncName[12]=BenchmarkSchemeL1Verify
 
 echo ${#benchmarkFuncName[@]}
 for((i=0;i<${#benchmarkFuncName[@]};i++));
@@ -49,8 +42,8 @@ do
   cat $filename | awk '{sum+=$2} END {print "#times = ", NR, ", Average = ", sum/NR, " ns"}'
 done
 
-#echo BenchmarkSchemeL1Verify
-#awk '$1 ~ /^'BenchmarkSchemeL1Verify'$/ {print $1,$3}' $file > ./$directory/BenchmarkSchemeL1Verify.txt
-#cat ./datafile/BenchmarkSchemeL1Verify.txt | awk '{sum+=$2} END {print "#times = ", NR, "Sum = ",sum, "Average = ", sum/NR, " ns"}'
+echo BenchmarkSchemeL1Verify
+awk '$1 ~ /^'BenchmarkSchemeL1Verify'$/ {print $1,$3}' $file > ./$directory/BenchmarkSchemeL1Verify.txt
+cat ./datafile/BenchmarkSchemeL1Verify.txt | awk '{sum+=$2} END {print "#times = ", NR, "Sum = ",sum, "Average = ", sum/NR, " ns"}'
 
 echo "=================== Part 3 extracting data done         ===============" 
